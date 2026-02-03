7ce1d8f8f0369a362392fde157d5f6037c838f7f
Rungrawee <jelly_pop3.2@hotmail.co.th>
2026-02-03 17:27:37 +0700

    chore: wrap app with cart provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from "./app/App";
-import CssBaseline from '@mui/material/CssBaseline'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./app/App"
+import { CartProvider } from "./context/CartContext"
+import CssBaseline from "@mui/material/CssBaseline"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <CartProvider>
+      <CssBaseline />
+      <App />
+    </CartProvider>
   </React.StrictMode>
 )
