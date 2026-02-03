feca15ce8e9fb45512ad106fc8c577db7fb9bb28
Rungrawee <jelly_pop3.2@hotmail.co.th>
2026-02-03 16:51:57 +0700

    fix: update App import path in main entry

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,27 @@
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav style={{ padding: '12px', borderBottom: '1px solid #ddd' }}>
-      <Link to="/" style={{ marginRight: '12px' }}>
-        Home
-      </Link>
-      <Link to="/menu" style={{ marginRight: '12px' }}>
-        Menu
-      </Link>
-      <Link to="/cart">
-        Cart
-      </Link>
-    </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          🦄 Unicorn Cafe
+        </Typography>
+
+        <Box>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/menu">
+            Menu
+          </Button>
+          <Button color="inherit" component={Link} to="/cart">
+            Cart
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
